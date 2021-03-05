d6e80d77ff97ea3d3e8f68fec1cb9317792f98ea
JoelKanyi <joelkanyi98@gmail.com>
2021-03-05 10:06:24 +0300

    maps

--- app/src/main/java/com/example/mydoctor/NearestFragment.java
+++ app/src/main/java/com/example/mydoctor/NearestFragment.java
@@ -1,160 +1,357 @@
 package com.example.mydoctor;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
 import com.android.volley.Request;
-import com.android.volley.Response;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.navigationview.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class NearestFragment extends Fragment implements OnMapReadyCallback, LocationListener {
-    GoogleMap map;
-    private static  Double lat= 0.6179084;
-    private static Double lng=34.5242683;
-    LocationManager locationmanager;
+import static com.example.mydoctor.AppConfig.GEOMETRY;
+import static com.example.mydoctor.AppConfig.GOOGLE_BROWSER_API_KEY;
+import static com.example.mydoctor.AppConfig.HOSPITAL_ID;
+import static com.example.mydoctor.AppConfig.ICON;
+import static com.example.mydoctor.AppConfig.LATITUDE;
+import static com.example.mydoctor.AppConfig.LOCATION;
+import static com.example.mydoctor.AppConfig.LONGITUDE;
+import static com.example.mydoctor.AppConfig.MIN_DISTANCE_CHANGE_FOR_UPDATES;
+import static com.example.mydoctor.AppConfig.MIN_TIME_BW_UPDATES;
+import static com.example.mydoctor.AppConfig.NAME;
+import static com.example.mydoctor.AppConfig.OK;
+import static com.example.mydoctor.AppConfig.PLACE_ID;
+import static com.example.mydoctor.AppConfig.PROXIMITY_RADIUS;
+import static com.example.mydoctor.AppConfig.REFERENCE;
+import static com.example.mydoctor.AppConfig.STATUS;
+import static com.example.mydoctor.AppConfig.VICINITY;
+import static com.example.mydoctor.AppConfig.ZERO_RESULTS;
 
-
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
-
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        }
-    };
+public class NearestFragment extends Fragment implements LocationListener, OnMapReadyCallback {
+    GoogleMap mMap;
+    private static Double lat = 0.0;
+    private static Double lng = 0.0;
+    CoordinatorLayout coordinatorLayout;
+    LocationManager locationManager;
+    private static final String TAG = "NearestFragment";
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_nearest, container, false);
+
         setHasOptionsMenu(true);
-        locationmanager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
-        if(!locationmanager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-            Toast.makeText(requireContext(), "Enable Location On Your Device", Toast.LENGTH_SHORT).show();
-            SupportMapFragment mapFragment =(SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
-        }
+
+        coordinatorLayout = view.findViewById(R.id.nearest_frag);
+        locationManager = (LocationManager) requireContext().getSystemService(Context.LOCATION_SERVICE);
+
+        Log.d(TAG, "onCreateView: "+locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER));
+        /*if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            showLocationSettings();
+        }*/
+
+        SupportMapFragment mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        assert mMapFragment != null;
+        mMapFragment.getMapAsync(this);
+
+        loadNearByPlaces(lat, lng);
+
         return view;
     }
 
-    private void loadNearbyHospitals(double lat, double lng){
+    private void showLocationSettings() {
+        Snackbar snackbar = Snackbar.make(coordinatorLayout, "Location Error: GPS Disabled!", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Enable", v -> startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)));
 
-        String type = "hospital_or_dispensary";
-        StringBuilder googlePlacesUrl =
+        snackbar.setActionTextColor(Color.RED);
+        TextView snackBarText = snackbar.getView().findViewById(com.google.android.material.R.id.snackbar_text);
+        snackBarText.setTextColor(Color.YELLOW);
+        snackbar.show();
+    }
 
-                new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
-        googlePlacesUrl.append("location=").append(lat).append(",").append(lng);
-        googlePlacesUrl.append("&radius=").append(AppConfig.PROXIMITY_RADIUS);
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        showCurrentLocation();
+    }
+
+    private void showCurrentLocation() {
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+
+        if (ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+
+        if (location != null) {
+            onLocationChanged(location);
+        }
+
+        locationManager.requestLocationUpdates(bestProvider, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+    }
+
+    private void loadNearByPlaces(double latitude, double longitude) {
+
+        String type = "hospital";
+        StringBuilder googlePlacesUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlacesUrl.append("location=").append(latitude).append(",").append(longitude);
+        googlePlacesUrl.append("&radius=").append(PROXIMITY_RADIUS);
         googlePlacesUrl.append("&types=").append(type);
         googlePlacesUrl.append("&sensor=true");
-        googlePlacesUrl.append("&key=" + AppConfig.GOOGLE_BROWSER_API_KEY);
+        googlePlacesUrl.append("&key=" + GOOGLE_BROWSER_API_KEY);
 
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,googlePlacesUrl.toString(),null,
-
-                new Response.Listener<JSONObject>() {
-                    @Override
-
-                    public void onResponse(JSONObject result) {
-
-                        Log.i(TAG, "onResponse: Result= " + result.toString());
-                        parseLocationResult(result);
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                     public void onErrorResponse(VolleyError error) {
-                        Log.e(TAG, "onErrorResponse: Error= " + error);
-                        Log.e(TAG, "onErrorResponse: Error= " + error.getMessage());
-                    }
-                });
-
+        @SuppressLint("LogNotTimber") JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, googlePlacesUrl.toString(), null,
+                this::parseLocationResult, error -> Log.d(TAG, "onErrorResponse: " + error.getMessage()));
 
         AppController.getInstance().addToRequestQueue(request);
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+    @SuppressLint("LogNotTimber")
+    private void parseLocationResult(JSONObject result) {
+
+        String placeName = null, vicinity = null;
+        double latitude, longitude;
+
+        try {
+            JSONArray jsonArray = result.getJSONArray("results");
+
+            if (result.getString(STATUS).equalsIgnoreCase(OK)) {
+
+                mMap.clear();
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject place = jsonArray.getJSONObject(i);
+
+                    if (!place.isNull(NAME)) {
+                        placeName = place.getString(NAME);
+                    }
+                    if (!place.isNull(VICINITY)) {
+                        vicinity = place.getString(VICINITY);
+                    }
+                    latitude = place.getJSONObject(GEOMETRY).getJSONObject(LOCATION).getDouble(LATITUDE);
+                    longitude = place.getJSONObject(GEOMETRY).getJSONObject(LOCATION).getDouble(LONGITUDE);
+
+                    LatLng latLng = new LatLng(latitude, longitude);
+                    Marker hospitals = mMap.addMarker (new MarkerOptions()
+                            .position(latLng)
+                            .title(placeName)
+                            .snippet(vicinity)
+                            .icon(BitmapDescriptorFactory.fromBitmap(getMarkerBitmapFromView(R.drawable.ic_hospital))));
+
+                    hospitals.showInfoWindow();
+                }
+
+                Toast.makeText(getContext(), jsonArray.length() + " Hospitals found!",Toast.LENGTH_LONG).show();
+            }
+
+        } catch (JSONException e) {
+            Log.e("TAG", e.getMessage());
         }
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
 
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.discovery_menu,menu);
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        LatLng latLng = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Hospital"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+
+        loadNearByPlaces(latitude, longitude);
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()== R.id.signOut){
-            Toast.makeText(requireContext(), "Sign out", Toast.LENGTH_SHORT).show();
+    public void onStatusChanged(String s, int i, Bundle bundle) { }
+
+    @Override
+    public void onProviderEnabled(String s) { }
+
+    @Override
+    public void onProviderDisabled(String s) { }
+
+    //Convert the custom marker icon to bitmap
+    private Bitmap getMarkerBitmapFromView(@DrawableRes int resId) {
+        View customMarkerView = ((LayoutInflater) requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.view_custom_marker, null);
+        ImageView markerImageView = (ImageView) customMarkerView.findViewById(R.id.profile_image);
+        markerImageView.setImageResource(resId);
+        customMarkerView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        customMarkerView.layout(0, 0, customMarkerView.getMeasuredWidth(), customMarkerView.getMeasuredHeight());
+        customMarkerView.buildDrawingCache();
+        Bitmap returnedBitmap = Bitmap.createBitmap(customMarkerView.getMeasuredWidth(), customMarkerView.getMeasuredHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+        canvas.drawColor(Color.WHITE, PorterDuff.Mode.SRC_IN);
+        Drawable drawable = customMarkerView.getBackground();
+        if (drawable != null)
+            drawable.draw(canvas);
+        customMarkerView.draw(canvas);
+        return returnedBitmap;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*SupportMapFragment mMapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        assert mMapFragment != null;
+        mMapFragment.getMapAsync(this);
+
+
+        return view;
+
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getContext());
+        if (ConnectionResult.SUCCESS == status) {
             return true;
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(status, getActivity(), 0).show();
+            return false;
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        *//*lat = location.getLatitude();
+        lng = location.getLongitude();*//*
+        LatLng latLng = new LatLng(lat, lng);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(12));
     }
 
     @Override
-    public void onLocationChanged(@NonNull Location location) {
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(@NonNull String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(@NonNull String provider) {
-
+        // TODO Auto-generated method stub
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
+        this.googleMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
+
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+        if (location != null) {
+            onLocationChanged(location);
+        }
+
+        locationManager.requestLocationUpdates(bestProvider, 20000, 0, this);
+
+        StringBuilder googlePlacesUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlacesUrl.append("location=" + lat + "," + lng);
+        googlePlacesUrl.append("&radius=" + PROXIMITY_RADIUS);
+        googlePlacesUrl.append("&types=" + "hospital");
+        googlePlacesUrl.append("&sensor=true");
+        googlePlacesUrl.append("&key=" + GOOGLE_API_KEY);
+
+        GooglePlacesReadTask googlePlacesReadTask = new GooglePlacesReadTask();
+        Object[] toPass = new Object[2];
+        toPass[0] = googleMap;
+        toPass[1] = googlePlacesUrl.toString();
+        googlePlacesReadTask.execute(toPass);
+
     }
-}
+
+}*/
